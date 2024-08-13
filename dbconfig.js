809dd4a909fe652ecfc80281ec64a6e3b55295f0
Sergio Flores <flores.ofiuco.sergio@gmail.com>
2024-08-12 19:02:42 -0500

    .

--- dbconfig.js
+++ dbconfig.js
@@ -3,19 +3,21 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    waitForConnections: true,
     connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
     if (err) throw err;
     console.log('Connected to the database!');
+    connection.release(); // Libera la conexión de vuelta al pool
 });
 
-
-module.exports = db;
+module.exports = pool;
